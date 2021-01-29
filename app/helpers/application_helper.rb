656cad1f7a8a18afeee116e712f85521d636a890
may arden <mayard3n@gmail.com>
2021-01-28 20:23:25 -0800

    added helper methods to offer user to login or sign up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+      
+    def render_nav_bar
+      if logged_in?
+        render partial: 'layouts/logged_in_links'
+      else
+        render partial: 'layouts/logged_out_links'
+      end
+    end
+
 end
