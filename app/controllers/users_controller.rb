76d1b02422e3494a23fd5d1731f33b4db5f991f2
may arden <mayard3n@gmail.com>
2021-01-28 20:00:03 -0800

    built user show functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
     def show
         # redirect_if_not_logged_in
         @user = User.find_by_id(params[:id])
-        # redirect to '/' if !@user
+        redirect to '/' if !@user
     end 
 
     private
