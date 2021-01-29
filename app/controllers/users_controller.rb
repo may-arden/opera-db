46845c7ee3e045230ee806bafed5506e6774f1c8
may arden <mayard3n@gmail.com>
2021-01-28 19:56:34 -0800

    built signup functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,17 @@
 class UsersController < ApplicationController
 
+    def index
+        @users = User.abc
+    end
+
     def new
+        @user = User.new 
     end 
 
     def create
         @user = User.new(user_params)
         if @user.save
-            session[:user_id] = @user.user_id
+            session[:user_id] = @user.id
             redirect_to user_path(@user)
         else 
             render :new
@@ -14,9 +19,9 @@ class UsersController < ApplicationController
     end 
 
     def show
-        redirect_if_not_logged_in
+        # redirect_if_not_logged_in
         @user = User.find_by_id(params[:id])
-        redirect to '/' if !@user
+        # redirect to '/' if !@user
     end 
 
     private
