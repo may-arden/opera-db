4b661f0c0ba62571e6ae5cd86834d1fe1e45c647
may arden <mayard3n@gmail.com>
2021-01-29 18:02:39 -0800

    built new song submission form & corresponding commands in songs controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,28 @@
 class SongsController < ApplicationController
+
+    def new
+      @song = Song.new
+       
+    end
+
+    def create
+      @song = Song.new(song_params)
+      @song.user_id = session[:user_id]
+        if @song.save
+          redirect_to song_path(@song)
+        else 
+          render :new  
+        end 
+    end 
+
+    def index 
+      @songs = Songs.all
+    end 
+
+    def show
+    end 
+
+    def edit
+    end 
+
 end
