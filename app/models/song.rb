3bd4917a97013271fe94b67046c59251b3f44e9c
may arden <mayard3n@gmail.com>
2021-01-30 16:50:50 -0800

    starting to build more strategically - seeded DB lightly increased song/new _form functionality

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,10 @@ class Song < ApplicationRecord
   belongs_to :user
   has_many :notes
   has_many :users, through: :notes 
+
+# instance method which displays the song title & the name of the opera it belongs to
+  def title_and_opera
+  end 
+
 end
+
