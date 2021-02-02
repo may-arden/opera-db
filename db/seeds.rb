7b71968bcd3ac48f49e1fcd86ffcda0e51037836
may arden <mayard3n@gmail.com>
2021-02-02 10:18:29 -0800

    deseeded database as i refine the associations between song & opera

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-operas = Opera.create([{name: 'la boheme'}])
+# operas = Opera.create([{name: 'la boheme'}])
 
-operas = Opera.create([{name: 'the magic flute'}, {name: 'i capuleti e i montecchi'}, {name: 'faust'}, 
-    {name: 'turandot'}, {name: 'einstein on the beach'}, {name: 'street scene'}, {name: 'madame butterfly'}, 
-    {name: 'norma'}, {name: 'les dialogues des carmelites'}, {name: 'don carlo'}])
+# operas = Opera.create([{name: 'the magic flute'}, {name: 'i capuleti e i montecchi'}, {name: 'faust'}, 
+#     {name: 'turandot'}, {name: 'einstein on the beach'}, {name: 'street scene'}, {name: 'madame butterfly'}, 
+#     {name: 'norma'}, {name: 'les dialogues des carmelites'}, {name: 'don carlo'}])
