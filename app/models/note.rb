4e471df13598042dd4e5672cf3a5c8529760c27f
may arden <mayard3n@gmail.com>
2021-02-03 12:17:26 -0800

    building note scope method

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,8 +3,19 @@ class Note < ApplicationRecord
   belongs_to :user
   validates :title, presence: true 
   validates :content, presence: true 
+  scope :published, -> { where(published: true)}
 
-  
+  def self.latest_note
+    order('published_at desc').first
+  end
+
+  def self.titles
+    pluck(:title)
+  end 
+
+  ## above scope method allows us to call
+  # Note.published.featured.latest_article
+  # Note.featured.title
 
 
 end
