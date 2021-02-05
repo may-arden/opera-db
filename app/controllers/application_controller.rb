f76f4f3cf4a56d73d3f45797ea4a987e9908a448
may arden <mayard3n@gmail.com>
2021-02-04 19:04:28 -0800

    DRYed up app controller by moving methods over to app helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,5 @@
 class ApplicationController < ActionController::Base
-
+    include ApplicationHelper 
     helper_method :current_user, :logged_in?
-
-    private
-
-    def logged_in?
-        !!session[:user_id]
-    end
-
-    def current_user
-        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
-    end
-
-    def redirect_if_not_logged_in
-        redirect_to '/' and return if !logged_in?
-    end 
-
-    def redirect_if_not_authorized
-        if @user != current_user
-            redirect_to '/' and return if !logged_in?
-        end 
-    end 
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,25 @@ module ApplicationHelper
         render partial: 'layouts/logged_in_links'
       else 
         render partial: 'layouts/logged_out_links'
+      end
     end
-  end
 
+    def logged_in?
+        !!session[:user_id]
+    end
 
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
+
+    def redirect_if_not_logged_in
+        redirect_to '/' and return if !logged_in?
+    end 
+
+    def redirect_if_not_authorized
+        if @user != current_user
+            redirect_to '/' and return if !logged_in?
+        end 
+    end 
 
 end
