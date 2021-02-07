8b1459e18cd309db39486c99d331a1eade59f703
may arden <mayard3n@gmail.com>
2021-02-07 12:10:54 -0800

    moved application controller methods from application helper back to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
     include ApplicationHelper 
     helper_method :current_user, :logged_in?
 
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    def current_user
+        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
+    end
+
+    def redirect_if_not_logged_in
+        redirect_to '/' and return if !logged_in?
+    end 
+
+    # def redirect_if_not_authorized ## not currently using this method 
+    #     if @user != current_user
+    #         redirect_to '/' and return if !logged_in?
+    #     end 
+    # end 
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,22 +8,4 @@ module ApplicationHelper
       end
     end
 
-    def logged_in?
-        !!session[:user_id]
-    end
-
-    def current_user
-        @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
-    end
-
-    def redirect_if_not_logged_in
-        redirect_to '/' and return if !logged_in?
-    end 
-
-    # def redirect_if_not_authorized ## not currently using this method 
-    #     if @user != current_user
-    #         redirect_to '/' and return if !logged_in?
-    #     end 
-    # end 
-
 end
